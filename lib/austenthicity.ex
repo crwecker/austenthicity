904bb641572b71a5f4b19750435f157e8350b7bf
Carl Wecker <crwecker@gmail.com>
2019-11-20 21:15:30 -0700

    Initial commit

---
+++ lib/austenthicity.ex
@@ -0,0 +1,18 @@
+defmodule Austenthicity do
+  @moduledoc """
+  Documentation for Austenthicity.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Austenthicity.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/austenthicity_test.exs
@@ -0,0 +1,8 @@
+defmodule AustenthicityTest do
+  use ExUnit.Case
+  doctest Austenthicity
+
+  test "greets the world" do
+    assert Austenthicity.hello() == :world
+  end
+end
